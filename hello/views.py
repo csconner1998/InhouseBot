c7c7b05cd7c4a260ddaa5db4e00a462dde3b35d2
Conner Soule <csconner1998@gmail.com>
2022-08-01 15:03:55 -0600

    Hopefully adding leaderboard.html

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import os
+import psycopg2
 from .models import Greeting
 
 # Create your views here.
@@ -8,9 +9,22 @@ def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
 
-
+def leaderboard(request):
+    host=os.environ.get('DB_HOST')
+    database=os.environ.get('DB_NAME')
+    user=os.environ.get('DB_USER')
+    password=os.environ.get('DB_PASS')
+    db_conn = psycopg2.connect(host=host, dbname=database, user=user, password=password)
+    db_cursor = db_conn.cursor()
+    cmd = "Select * from players;"
+    db_cursor.execute(cmd)
+    try:
+        array_players = db_cursor.fetchall()
+    except psycopg2.Error as e:
+        t_error_message = "Database error: " + e + "/n SQL: " + cmd
+        return render("error_report.html", t_error_message = t_error_message)
+    return render("results.html", t_title = "Test", array_softwares = array_players)
 def db(request):
-
     greeting = Greeting()
     greeting.save()
 
