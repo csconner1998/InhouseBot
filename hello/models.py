347c509105fbb662c15c7403ec2a66c3155b8a5c
Conner Soule <csconner1998@gmail.com>
2022-08-01 15:36:02 -0600

    Hopefully adding leaderboard.html

--- hello/models.py
+++ hello/models.py
@@ -4,4 +4,11 @@ from django.db import models
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
 
-    
+class Player():
+  def __init__(self, name, win, loss, ratio, sp):
+    self.name = name
+    self.win = win
+    self.loss = loss
+    self.ratio = ratio
+    self.sp = sp
+
