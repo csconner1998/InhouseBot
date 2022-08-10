a5dd899c1d4df9c80a3c13d8ae50443bbec06f24
Conner Soule <csconner1998@gmail.com>
2022-08-09 18:47:23 -0600

    Bug fix

--- bot.py
+++ bot.py
@@ -85,8 +85,6 @@ async def checkWinStr(reaction,user):
     await leaderboard.updateLeaderboard(reaction.message.channel, leaderboardMsgs=leaderboardMsgs, leaderboardChannel=leaderboardChannel, db_handler=db_handler)
     
 async def checkStart(message):
-    if len(message.reactions) < 12:
-        return ""
     cur = db_handler.getCursor()
     cmd = "select active_id from active_matches where react_msg_id = '"+str(message.id)+"'"
     cur.execute(cmd)
